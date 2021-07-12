1838aa2dbf2f50ac9d3f742cabd14239b2c5cbf7
matthieubrenne <matthieubrenne2103@gmail.com>
2021-07-12 11:39:46 +0200

    ajout font

--- screens/BookScreen.js
+++ screens/BookScreen.js
@@ -1,30 +1,40 @@
 import React from 'react'
 import { StyleSheet, View, Text, Image, ScrollView } from 'react-native'
 import { Card } from 'react-native-elements';
+import { useFonts } from 'expo-font';
+
 
 
 export default function BookScreen({ route }) {
 
     const book = route.params.book;
 
-    return (
-        <Card style={styles.card}>
-            <ScrollView>
-                <View style={styles.booksInfo}> 
-                    <Text style={styles.title}>{book.volumeInfo.title}</Text>   
-                    <Text style={styles.subtitle}>{book.volumeInfo.subtitle}</Text> 
-                    <Text style={styles.auteur}>Auteur(s) : {book.volumeInfo.authors}</Text>
-                    <Text style={styles.description}>Description : {book.volumeInfo.description}</Text> 
-                    <Image
-                        style={styles.img}
-                        source={{uri:book.volumeInfo.imageLinks.thumbnail}}
-                    />  
-                    <Text style={styles.date}>Date de parution: {book.volumeInfo.publishedDate}</Text>
-                     
-                </View>
-            </ScrollView>
-        </Card>
-    )
+    let [fontsloaded] = useFonts({
+        'AlfaSlabOne': require('../assets/fonts/AlfaSlabOne-Regular.ttf')
+    })
+
+    if(!fontsloaded) {
+        return <View><Text>Loading...</Text></View>
+    } else {
+        return (
+            <Card style={styles.card}>
+                <ScrollView>
+                    <View style={styles.booksInfo}> 
+                        <Text style={styles.title}>{book.volumeInfo.title}</Text>   
+                        <Text style={styles.subtitle}>{book.volumeInfo.subtitle}</Text> 
+                        <Text style={styles.auteur}>Auteur(s) : {book.volumeInfo.authors}</Text>
+                        <Text style={styles.description}>Description : {book.volumeInfo.description}</Text> 
+                        <Image
+                            style={styles.img}
+                            source={{uri:book.volumeInfo.imageLinks.thumbnail}}
+                        />  
+                        <Text style={styles.date}>Date de parution: {book.volumeInfo.publishedDate}</Text>
+                         
+                    </View>
+                </ScrollView>
+            </Card>
+        )
+    }
 }
 
 const styles = StyleSheet.create({
@@ -41,16 +51,19 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         fontSize: 20,
         textAlign: 'center',
-        marginVertical: 20
+        marginVertical: 20,
+        fontFamily: 'AlfaSlabOne'
     },
     subtitle: {
         fontSize: 15,
         textAlign: 'center',
-        fontWeight: '500'
+        fontFamily: 'AlfaSlabOne'
+
     }, 
     date: {
         textAlign: 'center',
-        fontStyle: 'italic'
+        fontStyle: 'italic',
+        fontFamily: 'AlfaSlabOne'
     },
     auteur: {
         textAlign: 'center',
